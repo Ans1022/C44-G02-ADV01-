bc73253daadaf8cef10c1210648f7c9bae53acfa
Anas Madkour <anas.mohamed0976@gmail.com>
2025-08-07 20:40:02 +0300

    Add project files.

---
+++ Assignmentt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignmentt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
